776efacf0c3f853eda42e8ebf17415b23efe85c4
ccHarvestasya <harvestasya84@gmail.com>
2026-09-06 11:43:15 +0900

    fix: [packages/wallet-core] Android identity lookup の namespace を修正
    - JNI_OnLoad から package namespace の artifact identity symbols を参照する

--- packages/wallet-core/android/OnLoad.cpp
+++ packages/wallet-core/android/OnLoad.cpp
@@ -58,8 +58,9 @@ std::shared_ptr<TurboModule> javaModuleProvider(
 } // namespace facebook::react
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
-  if (snwc_rn_module_identity() == nullptr || snwc_rn_artifact_identity() == nullptr ||
-      snwc_rn_target_id() == nullptr) {
+  if (facebook::react::snwc_rn_module_identity() == nullptr ||
+      facebook::react::snwc_rn_artifact_identity() == nullptr ||
+      facebook::react::snwc_rn_target_id() == nullptr) {
     return JNI_ERR;
   }
   facebook::react::RnLifecycleCoordinator::shared().registerProcessLifecycle();
